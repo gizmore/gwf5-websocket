32bd5df0c1b905764ea46a3c6accb72eb77deda1
gizmore <gizmore@wechall.net>
2017-07-26 13:06:01 +0200

    GDO_Secret-for-hidden-configs

--- websocket/GWS_Register.php
+++ websocket/GWS_Register.php
@@ -8,9 +8,7 @@ final class GWS_Register extends GWS_CommandForm
 	
 	public function replySuccess(GWS_Message $msg, GWF_Form $form, GWF_Response $response)
 	{
-		GWF_User::$CURRENT = $user = GWF_Session::instance()->getUser();
-		GWF_Session::reset();
-		$msg->replyBinary($msg->cmd(), $this->userToBinary($user));
+		$msg->replyBinary($msg->cmd());
 	}
 }
 
